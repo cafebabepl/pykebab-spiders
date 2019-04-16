78c54a1f730e14701415a4278d0da31dd68ad7ca
Włodzimierz Kozłowski <wlodzimierz@kozlowscy.org>
2019-04-16 16:05:25 +0200

    usunięcie zbędnych komentarzy i print

--- pykebab/spiders/CamelPizza.py
+++ pykebab/spiders/CamelPizza.py
@@ -13,14 +13,14 @@ class CamelPizzaSpider(scrapy.Spider):
 
         for groupListItem in response.css('div.m-group__list-item'):
             grupa = get_text(groupListItem.css('h3.m-list__title'))
-            print('---', grupa, '---')
+            #print('---', grupa, '---')
 
             if grupa.lower() in ['sosy', 'napoje']:
                 continue
 
             for itemRow in groupListItem.css('div.m-item__row'):
                 pozycja = get_text(itemRow.css('h4.m-item__title'))
-                print("-->", pozycja)
+                #print("-->", pozycja)
                 opis = get_text(itemRow.css('div.m-item__description'))
                 addToCartButton = itemRow.css('.js-add-to-cart-button')
 
@@ -29,12 +29,12 @@ class CamelPizzaSpider(scrapy.Spider):
                     if 'kebap' in grupa.lower() and u'mięso' in opis.lower():
                         for wariant in [u'wołowina', u'kurczak']:
                             item = MenuItem(grupa = grupa, pozycja = pozycja, opis = opis, wariant = wariant, cena = cena)
-                            print(item)
+                            #print(item)
                             yield item                    
                     else:
                         wariant = ''
                         item = MenuItem(grupa = grupa, pozycja = pozycja, opis = opis, wariant = wariant, cena = cena)
-                        print(item)
+                        #print(item)
                         yield item
                 elif len(addToCartButton) > 1:
                     for addToCartItem in addToCartButton[1:]:
@@ -42,44 +42,5 @@ class CamelPizzaSpider(scrapy.Spider):
                         wariant = pola[0].strip()
                         cena = pola[-1].strip()
                         item = MenuItem(grupa = grupa, pozycja = pozycja, opis = opis, wariant = wariant, cena = cena)
-                        print(item)
+                        #print(item)
                         yield item
-
-'''
-    def parse(self, response):
-
-        def get_text(x):
-            return ''.join(x.css('::text').extract()).strip()
-			
-        grupa = ''.join(response.css('h3.m-list__title').css('::text').extract()).strip()
-        print("------------------------------------", grupa)
-
-        grupa = 'nieznana'
-
-        for x in response.css('div.m-item__row'):
-            title = get_text(x.css('h4.m-item__title'))
-            print("----------->", title)
-
-            description = get_text(x.css('div.m-item__description'))
-            ceny = x.css('.js-add-to-cart-button')
-
-            pozycja = title
-            wariant = ''
-
-            if len(ceny) == 1:
-                cena = get_text(ceny[0])
-                item = MenuItem(grupa = grupa, pozycja = pozycja, wariant = wariant, cena = cena)
-                print(item)
-                yield item
-            elif len(ceny) > 1:
-                for i in range(1, len(ceny)):
-                    cena = get_text(ceny[i])
-                    wariant = 'wariant' + str(i)
-                    item = MenuItem(grupa = grupa, pozycja = pozycja, wariant = wariant, cena = cena)
-                    print(item)
-                    yield item
-
-            #xxx = len(x.css('.js-add-to-cart-button'))            
-            #cena = get_text(x.css('.js-add-to-cart-button'))
-'''         
-            
